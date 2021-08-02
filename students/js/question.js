9ba2a08adc122dbd2d3e36fc428d0ea74260019d
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-08-02 18:13:07 +0600

    Add More Question

--- students/js/question.js
+++ students/js/question.js
@@ -3,6 +3,33 @@ const question2 = document.querySelector(".question-2");
 const question3 = document.querySelector(".question-3");
 const question4 = document.querySelector(".question-4");
 const question5 = document.querySelector(".question-5");
+const question6 = document.querySelector(".question-6");
+const question7 = document.querySelector(".question-7");
+const question8 = document.querySelector(".question-8");
+const question9 = document.querySelector(".question-9");
+const question10 = document.querySelector(".question-10");
+const question11 = document.querySelector(".question-11");
+const question12 = document.querySelector(".question-12");
+const question13 = document.querySelector(".question-13");
+const question14 = document.querySelector(".question-14");
+const question15 = document.querySelector(".question-15");
+const question16 = document.querySelector(".question-16");
+const question17 = document.querySelector(".question-17");
+const question18 = document.querySelector(".question-18");
+const question19 = document.querySelector(".question-19");
+const question20 = document.querySelector(".question-20");
+const question21 = document.querySelector(".question-21");
+const question22 = document.querySelector(".question-22");
+const question23 = document.querySelector(".question-23");
+const question24 = document.querySelector(".question-24");
+const question25 = document.querySelector(".question-25");
+const question26 = document.querySelector(".question-26");
+const question27 = document.querySelector(".question-27");
+const question28 = document.querySelector(".question-28");
+const question29 = document.querySelector(".question-29");
+const question30 = document.querySelector(".question-30");
+const question31 = document.querySelector(".question-31");
+const question32 = document.querySelector(".question-32");
 
 
 // For Next
@@ -28,6 +55,141 @@ document.querySelector("#q4-next").addEventListener("click", () => {
 
 document.querySelector("#q5-next").addEventListener("click", () => {
     question5.style.display = "none";
+    question6.style.display = "block";
+});
+
+document.querySelector("#q6-next").addEventListener("click", () => {
+    question6.style.display = "none";
+    question7.style.display = "block";
+});
+
+document.querySelector("#q7-next").addEventListener("click", () => {
+    question7.style.display = "none";
+    question8.style.display = "block";
+});
+
+document.querySelector("#q8-next").addEventListener("click", () => {
+    question8.style.display = "none";
+    question9.style.display = "block";
+});
+
+document.querySelector("#q9-next").addEventListener("click", () => {
+    question9.style.display = "none";
+    question10.style.display = "block";
+});
+
+document.querySelector("#q10-next").addEventListener("click", () => {
+    question10.style.display = "none";
+    question11.style.display = "block";
+});
+
+document.querySelector("#q11-next").addEventListener("click", () => {
+    question11.style.display = "none";
+    question12.style.display = "block";
+});
+
+document.querySelector("#q12-next").addEventListener("click", () => {
+    question12.style.display = "none";
+    question13.style.display = "block";
+});
+
+document.querySelector("#q13-next").addEventListener("click", () => {
+    question13.style.display = "none";
+    question14.style.display = "block";
+});
+
+document.querySelector("#q14-next").addEventListener("click", () => {
+    question14.style.display = "none";
+    question15.style.display = "block";
+});
+
+document.querySelector("#q15-next").addEventListener("click", () => {
+    question15.style.display = "none";
+    question16.style.display = "block";
+});
+
+document.querySelector("#q16-next").addEventListener("click", () => {
+    question16.style.display = "none";
+    question17.style.display = "block";
+});
+
+document.querySelector("#q17-next").addEventListener("click", () => {
+    question17.style.display = "none";
+    question18.style.display = "block";
+});
+
+document.querySelector("#q18-next").addEventListener("click", () => {
+    question18.style.display = "none";
+    question19.style.display = "block";
+});
+
+document.querySelector("#q19-next").addEventListener("click", () => {
+    question19.style.display = "none";
+    question20.style.display = "block";
+});
+
+document.querySelector("#q20-next").addEventListener("click", () => {
+    question20.style.display = "none";
+    question21.style.display = "block";
+});
+
+document.querySelector("#q21-next").addEventListener("click", () => {
+    question21.style.display = "none";
+    question22.style.display = "block";
+});
+
+document.querySelector("#q22-next").addEventListener("click", () => {
+    question22.style.display = "none";
+    question23.style.display = "block";
+});
+
+document.querySelector("#q23-next").addEventListener("click", () => {
+    question23.style.display = "none";
+    question24.style.display = "block";
+});
+
+document.querySelector("#q24-next").addEventListener("click", () => {
+    question24.style.display = "none";
+    question25.style.display = "block";
+});
+
+document.querySelector("#q25-next").addEventListener("click", () => {
+    question25.style.display = "none";
+    question26.style.display = "block";
+});
+
+document.querySelector("#q26-next").addEventListener("click", () => {
+    question26.style.display = "none";
+    question27.style.display = "block";
+});
+
+document.querySelector("#q27-next").addEventListener("click", () => {
+    question27.style.display = "none";
+    question28.style.display = "block";
+});
+
+document.querySelector("#q28-next").addEventListener("click", () => {
+    question28.style.display = "none";
+    question29.style.display = "block";
+});
+
+document.querySelector("#q29-next").addEventListener("click", () => {
+    question29.style.display = "none";
+    question30.style.display = "block";
+});
+
+document.querySelector("#q30-next").addEventListener("click", () => {
+    question30.style.display = "none";
+    question31.style.display = "block";
+});
+
+document.querySelector("#q31-next").addEventListener("click", () => {
+    question31.style.display = "none";
+    question32.style.display = "block";
+});
+
+document.querySelector("#q32-next").addEventListener("click", () => {
+    question32.style.display = "none";
 });
 
 // For Previous
@@ -49,4 +211,139 @@ document.querySelector("#q4-pre").addEventListener("click", () => {
 document.querySelector("#q5-pre").addEventListener("click", () => {
     question5.style.display = "none";
     question4.style.display = "block";
+});
+
+document.querySelector("#q6-pre").addEventListener("click", () => {
+    question6.style.display = "none";
+    question5.style.display = "block";
+});
+
+document.querySelector("#q7-pre").addEventListener("click", () => {
+    question7.style.display = "none";
+    question6.style.display = "block";
+});
+
+document.querySelector("#q8-pre").addEventListener("click", () => {
+    question8.style.display = "none";
+    question7.style.display = "block";
+});
+
+document.querySelector("#q9-pre").addEventListener("click", () => {
+    question9.style.display = "none";
+    question8.style.display = "block";
+});
+
+document.querySelector("#q10-pre").addEventListener("click", () => {
+    question10.style.display = "none";
+    question9.style.display = "block";
+});
+
+document.querySelector("#q11-pre").addEventListener("click", () => {
+    question11.style.display = "none";
+    question10.style.display = "block";
+});
+
+document.querySelector("#q12-pre").addEventListener("click", () => {
+    question12.style.display = "none";
+    question11.style.display = "block";
+});
+
+document.querySelector("#q13-pre").addEventListener("click", () => {
+    question13.style.display = "none";
+    question12.style.display = "block";
+});
+
+document.querySelector("#q14-pre").addEventListener("click", () => {
+    question14.style.display = "none";
+    question13.style.display = "block";
+});
+
+document.querySelector("#q15-pre").addEventListener("click", () => {
+    question15.style.display = "none";
+    question14.style.display = "block";
+});
+
+document.querySelector("#q16-pre").addEventListener("click", () => {
+    question16.style.display = "none";
+    question15.style.display = "block";
+});
+
+document.querySelector("#q17-pre").addEventListener("click", () => {
+    question17.style.display = "none";
+    question16.style.display = "block";
+});
+
+document.querySelector("#q18-pre").addEventListener("click", () => {
+    question18.style.display = "none";
+    question17.style.display = "block";
+});
+
+document.querySelector("#q19-pre").addEventListener("click", () => {
+    question19.style.display = "none";
+    question18.style.display = "block";
+});
+
+document.querySelector("#q20-pre").addEventListener("click", () => {
+    question20.style.display = "none";
+    question19.style.display = "block";
+});
+
+document.querySelector("#q21-pre").addEventListener("click", () => {
+    question21.style.display = "none";
+    question20.style.display = "block";
+});
+
+document.querySelector("#q22-pre").addEventListener("click", () => {
+    question22.style.display = "none";
+    question21.style.display = "block";
+});
+
+document.querySelector("#q23-pre").addEventListener("click", () => {
+    question23.style.display = "none";
+    question22.style.display = "block";
+});
+
+document.querySelector("#q24-pre").addEventListener("click", () => {
+    question24.style.display = "none";
+    question23.style.display = "block";
+});
+
+document.querySelector("#q25-pre").addEventListener("click", () => {
+    question25.style.display = "none";
+    question24.style.display = "block";
+});
+
+document.querySelector("#q26-pre").addEventListener("click", () => {
+    question26.style.display = "none";
+    question25.style.display = "block";
+});
+
+document.querySelector("#q27-pre").addEventListener("click", () => {
+    question27.style.display = "none";
+    question26.style.display = "block";
+});
+
+document.querySelector("#q28-pre").addEventListener("click", () => {
+    question28.style.display = "none";
+    question27.style.display = "block";
+});
+
+document.querySelector("#q29-pre").addEventListener("click", () => {
+    question29.style.display = "none";
+    question28.style.display = "block";
+});
+
+document.querySelector("#q30-pre").addEventListener("click", () => {
+    question30.style.display = "none";
+    question29.style.display = "block";
+});
+
+document.querySelector("#q31-pre").addEventListener("click", () => {
+    question31.style.display = "none";
+    question30.style.display = "block";
+});
+
+document.querySelector("#q32-pre").addEventListener("click", () => {
+    question32.style.display = "none";
+    question31.style.display = "block";
 });
